c06efcb1e4314819b73eb1c25ab22f537ebc8150
Alejandro Paz <alepazte@hotmail.com>
2021-01-04 00:17:59 -0600

    feat: create component using deprecated SFC interface

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById("root"));
+type AppProps = { message: string };
+const App: React.SFC<AppProps> = ({ message }) => <div>{message}</div>;
+
+ReactDOM.render(<App message="Hello World" />, document.getElementById("root"));
